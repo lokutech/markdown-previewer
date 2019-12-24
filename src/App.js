09459702f4c9f544c870123dbebdb10e36b50050
lokutech <onatim@protonmail.com>
2019-12-24 22:25:07 +0300

    Fully functional. Passing tests except the bonus. Needs styling.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import React from 'react'
 import './App.css'
+import marked from 'marked'
+
+const initialValue = `
+# Header
+## Sub Header 
+This is a [link](https://www.freecodecamp.com)
+**Some cool bold text**
+> Some block quote\n
+Here is some \`code\`
+
+\nthis is multi-line code\n\n
+
+\`\`\`
+function anotherExample (firstLine, lastLine){
+  if (firstLine == "" && lastLine == "") {
+    return multiLineCode;}
+  }
+\`\`\`
+
+- List item 1
+- List item 2
+![React Logo w/ Text](https://goo.gl/Umyytc)`
 
 class App extends React.Component {
   state = {
-    input: ''
+    input: initialValue,
+    marked: ''
   }
 
   handleChange = e => {
     this.setState({
-      input: e.target.value
+      input: e.target.value,
+      marked : marked(e.target.value)
+    })
+  }
+
+  componentDidMount = () =>{
+    this.setState({
+      marked : marked(this.state.input)
     })
   }
 
   render() {
     return (
       <div>
-
         <textarea
           id='editor'
           value={this.state.input}
-          onChange={this.handleChange}></textarea>
+          onChange={this.handleChange}
+          defaultValue='#Header'>Default value</textarea>
 
-        <p id='preview'>{this.state.input}</p>
+        <p id='preview' 
+           dangerouslySetInnerHTML= {{__html: this.state.marked}}
+           ></p>
 
       </div>
     )
